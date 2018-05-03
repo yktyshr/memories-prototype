159d9a9826033e3bf4582d4225fafda0b32f3f0f
gosho-kazuya <ketsume0211@gmail.com>
2018-05-03 18:45:32 +0900

    add editorconfig

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      query: 'a',
+      searchResults: {
+        google: [],
+      },
+    }
+  }
+
+  _onUpdateQuery = (event) => {
+    this.setState({ query: event.target.value })
+    this.searchResults = 'https://www.google.co.jp/search?'
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Memories</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-body">
+          <div className="App-body-note">
+            <input
+              defaultValue={this.state.query}
+              onChange={this._onUpdateQuery}
+            />
+          </div>
+          <div className="App-body-search">
+            {this.state.query}
+          </div>
+          <iframe src="https://www.google.co.jp/search?q=hoge" title="iframe example 1" width="400" height="300">
+            <p>Your browser does not support iframes.</p>
+          </iframe>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
